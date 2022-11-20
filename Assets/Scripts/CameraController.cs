f07f6a6b14df8b60ef564bc13981cc7146e84190
Miguel Alejandro Santiago Perez <85505816+MikeLordx@users.noreply.github.com>
2022-11-20 12:40:48 -0600

    Controllers

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float _mouseSensitivityX = default;
+    [SerializeField] private float _mouseSensitivityY = default;
+    [SerializeField] private Transform _player = default;
+    [SerializeField] private Transform _target = default;
+    private float _xRotation = 0.0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        transform.LookAt(_target);
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivityX * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivityY * Time.deltaTime;
+
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        _player.Rotate(Vector3.up * mouseX);
+    }
+}
